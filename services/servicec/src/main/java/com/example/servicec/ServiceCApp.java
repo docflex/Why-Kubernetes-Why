831c11616aad77899b91d3ae933106e3ad096d70
Rehber Moin <rehbermoin.io@gmail.com>
2025-11-16 11:01:55 +0530

    ADDED: Service C

---
+++ services/servicec/src/main/java/com/example/servicec/ServiceCApp.java
@@ -0,0 +1,11 @@
+package com.example.servicec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCApp.class, args);
+    }
+}
